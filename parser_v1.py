5b730c7b91e3440a64b4db7adba32a2a0a19f6f9
Sebastien Lelong <slelong@scripps.edu>
2019-11-01 09:18:18 -0700

    v1 using plugin

---
+++ parser_v1.py
@@ -0,0 +1,13 @@
+import os, pandas
+
+def load_data(data_folder):
+    products_file = os.path.join(data_folder,"Products.csv")
+    assert os.path.exists(products_file)
+    dat = pandas.read_csv(products_file,squeeze=True).to_dict(orient='records')
+    for rec in dat:
+        rec["_id"] = str(rec["NDB_Number"])
+        #rec["manufacturer"] = str(rec["manufacturer"])
+        #rec["ingredients_english"] = str(rec["ingredients_english"])
+        #rec.pop("gtin_upc",None)
+        yield rec
+
